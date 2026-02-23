dd6c8b1b767c0476e18a1b70838d6b6594df94bf
Wangyaqi <ccc123zhy@outlook.com>
2026-02-23 11:42:23 +0800

    优化日志记录器设置，添加关于LOG_PATH可能不存在的注释说明

--- app/log.py
+++ app/log.py
@@ -7,6 +7,8 @@ import os
 def setup_logger(app: Flask):
     '''
     设置日志记录器。
+    可能的问题：
+        LOG_PATH 不存在可能抛出异常，但在实际使用时，会在调用该函数前运行 init_files 函数，确保 LOG_PATH 存在。
     Arguments:
         app: Flask 当前的Flask应用实例。
     Returns:
@@ -39,6 +41,6 @@ def setup_logger(app: Flask):
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
     
-    app.logger = logger
+    app.logger = logger # type: ignore  
 
     return logger
